3641928d8f99b782cce924e26a67c8a79c0f9984
vc <yinlaile@gmail.com>
2025-12-14 07:38:28 +0800

    更改ss源

--- get_v2ray_sub.py
+++ get_v2ray_sub.py
@@ -1,5 +1,45 @@
 #!/usr/bin/python3
 
+'''
+两个目标
+1,从alvin9999提供的html页面，提取 uri，整理成 v2ray订阅
+2,从上一步和其它人的分享的v2ray订阅中，整理 ss 和 ssr 订阅
+
+背景
+常见加密协议 shadowsocks(ss), shadowsocksR(ssr), xray(vless, vmess, torjan)
+
+URI,对应协议。
+类似网址，提供配置信息，定位到单一服务器地址，格式为"ss://, ssr://, vmess://..."
+
+订阅 多个服务器配置集合，对应客户端。
+常见 base64文本，(ssr, v2ray, karing, shadowrocket)，yaml(clash),json(ss,
+singbox)
+
+从客户端，新出的app更先进，支持的协议多，更多功能也多，如一键测速，自动更新订阅，分流。
+
+对比之下，原生的ss和ssr客户端，都只支持各自对应协议，较为单一。且ss不支持局域网共享。
+ssr作者已经删库了，不再更新了。ss虽有更新，但不频频繁。
+优点，占用资源少。内存，电池用量（手机端比v2ray显著节能），且感觉ssr体验更稳定。
+
+采用的策略是，把 ss 和 ssr当主用, 而把v2ray当备用。
+
+shadowsocks 订阅，有官方文档可查。
+#https://shadowsocks.org/doc/sip008.html
+
+ssr 订阅，难以查看文档。但是，在windows版本ssr程序上，自带了
+一个指向github订阅网址。本身已经失效了，但是根据地址路径，搜索到github山第三方备份。
+https://github.com/shadowsocksr-rm/breakwa11.github.io/blob/master/free
+/freenodeplain.txt
+
+虽然这份订阅也已不再更新。但是提供了两个信息，第一，ssr用的base文本，而非ss的json。
+第二，订阅中uri格式更严格。要求必须有group。
+153.125.233.236:31461:origin:aes-256-cfb:plain:bnRkdHYuY29t/?obfsparam=&
+remarks=6ZmQ6YCf5biQ5Y-3IEFsdmluOTk5OQ&group=RnJlZVNTUi1wdWJsaWM
+
+另外，ssr app 虽然支持手动导入 ss uri,但是似乎并不能正常使用，且订阅示例中，只有ssr，
+没有ss,故没再深究是否支持ss订阅。
+'''
+
 import requests;
 import re;
 import base64;
@@ -7,15 +47,16 @@ import json
 import uuid;
 import urllib.parse
 
+
 ss_url_github = "https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7";
 v2ray_url_github = "https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7";
 #url = "https://gitlab.com/zhifan999/fq/-/wikis/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
 #url = "https://s3.dualstack.us-west-2.amazonaws.com/zhifan2/wiki.html"
 
-#
-ssv2_url_fn = "https://proxy.v2gh.com/https://raw.githubusercontent.com/free-nodes/v2rayfree/main/v2"
-
-
+#实测 uri多，但是基本都是无效
+#ssv2_url_fn = "https://proxy.v2gh.com/https://raw.githubusercontent.com/free-nodes/v2rayfree/main/v2"
+#实测 虽然广告多，可用，速度高
+ssv2_url_fn="https://raw.githubusercontent.com/ssrsub/ssr/master/v2ray";
 
 def write_to_local(file_name, content):
     f = open(file_name, "w");
@@ -35,7 +76,8 @@ def trans_uri2cfg(ss_uri):
 	#SS-URI = "ss://" userinfo "@" hostname ":" port [ "/" ] [ "?" plugin ] [ "#" tag ]
 	#userinfo = websafe-base64-encode-utf8(method  ":" password)
 	#           method ":" password 
-	# hostname ipv4 111.22.22.33:2345, ipv6 [2001:bc8:32d7:2013::10]:1111，提取ipv6地址需注意包围[]
+	# hostname ipv4 111.22.22.33:2345, 
+	#ipv6 [2001:bc8:32d7:2013::10]:1111，提取ipv6地址需注意包围[]
 	mPattern = r"ss://(?P<userinfo>[\w=+-]+)@\[?(?P<hostname>[A-Za-z0-9:.]+)\]?:(?P<port>[A-Za-z0-9:.]+)#(?P<tag>.+)"
 	matched = re.search(mPattern, ss_uri)
 	#print(matched.group("hostname"))
@@ -68,7 +110,8 @@ def add_ssr_group(ssr_url):
 	.decode("utf-8");    
 	#根据模板，添加"group"属性
 	plain_content += "&group=eXVuZmFu"; #yunfan    
-	return "ssr://" + base64.urlsafe_b64encode(plain_content.encode("utf-8")).decode("utf-8");            
+	return "ssr://" + base64.urlsafe_b64encode(
+	plain_content.encode("utf-8")).decode("utf-8");            
 
 def main():
 
@@ -82,7 +125,8 @@ def main():
     
     while len(fn_uri_ec) % 4 != 0:
         fn_uri_ec += "=";
-    fn_ss_text = base64.urlsafe_b64decode(fn_uri_ec.encode("utf-8")).decode("utf-8")
+    fn_ss_text = base64.urlsafe_b64decode(
+    fn_uri_ec.encode("utf-8")).decode("utf-8")
 
     fn_ss_list = fn_ss_text.split("\n");
     fn_ss_iter = filter(lambda s : re.match("ss://", s), fn_ss_list)
@@ -118,9 +162,6 @@ def main():
     alv_ssr_uris = [ add_ssr_group(ssr) for ssr in alv_ssr_uris]
 
     #ss_urls = filter ( lambda url : re.match("ss://", url) ,  ssr_urls )
-
-    print(f"{len(alv_v2_mix)} v2ray_uri, {len(alv_ss_ssr)} ssr_uri\
-    {len(fn_ss_iter)} fn_ss_uri")
     
     #将 ss-uri 转换成 json 格式 config    
     ss_cfg_list = []
@@ -135,17 +176,18 @@ def main():
     #sub_urls = [u.replace("&amp;", "&")  for u in urls ]
     #print(sub_urls)
     #ss_pure_sub = base64.b64encode("\n".join(alv_ss_uris).encode("utf-8")).decode("utf-8");
-    ssr_sub = base64.b64encode("\n".join(alv_ssr_uris).encode("utf-8")).decode("utf-8");
-    #ss+ssr 
-    ss_ssr_sub = base64.b64encode("\n".join(alv_ss_ssr).encode("utf-8")).decode("utf-8");
-    #ss, ssr, v2
-    v2_mix_sub = base64.b64encode("\n".join(alv_ss_ssr+ alv_v2_mix)\
+    ssr_sub = base64.urlsafe_b64encode("\n".join(alv_ssr_uris)\
     .encode("utf-8")).decode("utf-8");
+    #ss, ssr, v2
+    v2_mix_sub = base64.urlsafe_b64encode("\n".join(alv_ss_ssr + 
+    alv_v2_mix).encode("utf-8")).decode("utf-8");
+
+    print(f"{len(ss_cfg_list)} ss cfg, {len(alv_ssr_uris)} ssr,\
+    {len(alv_v2_mix)} v2ray")    
     write_to_local("ss-cfg.json", cfg_json)
     write_to_local("ssr-pure.txt", ssr_sub)
-    #write_to_local("ss-ssr.txt", ss_ssr_sub)
+
     write_to_local("v2-mix.txt", v2_mix_sub)
 
 if __name__ == "__main__":
     main();
-
